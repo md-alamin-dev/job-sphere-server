eb040f42df49263dbf5001a6bdf76696712e2802
md-alamin-dev <alamin.dev99@gmail.com>
2024-10-03 17:45:49 +0600

    Get all reviews

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmvse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,16 +25,29 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const reviewCollection = client.db("jobSphereDB").collection("reviews");
+
+        
+        // Get all reviews
+        app.get('/reviews', async (req, res) => {
+
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+    }
+    finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
-run().catch(console.dir);
 
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
